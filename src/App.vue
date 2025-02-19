ec98900ba9d2575ad9fdefa817af43b0283fb0d3
Jake Landrum <jakelandrum2004@gmail.com>
2025-02-19 17:15:33 -0600

    projectsFinished

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <script setup>
+import { ref } from 'vue'
 import projectData from './data/projects.json'
 import technicalSkillsData from './data/technicalskills.json'
+
+const showProjects = ref(false) // use ref to live update the var for toggling active element
 
 function GoToLink(link) {
   window.open(link, '_blank')
 }
 
 function scrollToSection(elementId) {
-  const element = document.getElementById(elementId);
+  const element = document.getElementById(elementId)
   if (element) {
-    element.scrollIntoView({behavior: "smooth"});
+    element.scrollIntoView({behavior: "smooth"})
   }
 }
 </script>
@@ -18,7 +21,8 @@ function scrollToSection(elementId) {
 <template>
   <link rel="stylesheet" type='text/css' href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css" />
   
-  <div class="h-screen w-screen flex items-center justify-center overflow-x-hidden overflow-y-hidden">
+  <!--Main page-->
+  <div v-if="!showProjects" class="h-screen w-screen flex items-center justify-center overflow-x-hidden overflow-y-hidden">
     <div class="w-screen h-screen grid grid-cols-2">
       <!-- LEFT SIDE -->
       <div class="text-white p-6 flex flex-col lg:pl-40">
@@ -34,9 +38,10 @@ function scrollToSection(elementId) {
         </div>
 
         <div class="mt-auto flex space-x-4 pt-10">
-          <img @click="GoToLink('https://www.linkedin.com/in/jakealandrum/')" src="https://img.icons8.com/?size=100&id=8808&format=png&color=737373" class="w-8 h-8 hover:cursor-pointer">
-          <img @click="GoToLink('https://github.com/jakeyboi1')" src="https://img.icons8.com/?size=100&id=RHLuYrY4GjUv&format=png&color=737373" class="w-8 h-8 hover:cursor-pointer">
-        </div>
+          Contact me: &nbsp;
+          <img @click="GoToLink('https://www.linkedin.com/in/jakealandrum/')" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linkedin/linkedin-original.svg" class="w-8 h-8 hover:cursor-pointer">
+          <i @click="GoToLink('https://github.com/jakeyboi1')" class="devicon-github-original-wordmark hover:cursor-pointer" style="font-size: xx-large;"></i>
+          </div>
       </div>
 
       <!-- RIGHT SIDE -->
@@ -69,7 +74,7 @@ function scrollToSection(elementId) {
               <br>
             </div>
           </div>
-          <button class="pt-4 text-left font-bold hover:opacity-80 hover:cursor-pointer flex">
+          <button class="pt-4 text-left font-bold hover:opacity-80 hover:cursor-pointer flex" @click="showProjects = true">
             View All Projects
             <svg class="w-4 ml-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25"><path style="fill:#FFFFFF" d="m17.5 5.999-.707.707 5.293 5.293H1v1h21.086l-5.294 5.295.707.707L24 12.499l-6.5-6.5z" data-name="Right"/></svg>
           </button> 
@@ -88,7 +93,48 @@ function scrollToSection(elementId) {
       </div>
     </div>
   </div>
+
+  <!--All Projects Page-->
+  <!--I could use Vue Router for this and actually make different pages but considering I only have 2 its just not needed the ref var works fine-->
+  <div v-if="showProjects" class="h-screen w-screen overflow-x-hidden">
+    <div class="w-full h-full">
+      <button @click="showProjects = false" class="flex text-lg pt-2 pl-2 text-gray-400 hover:text-white hover:cursor-pointer">
+        <svg class="w-4 ml-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25"><path style="fill:#FFFFFF" d="M24 12.001H2.914l5.294-5.295-.707-.707L1 12.501l6.5 6.5.707-.707-5.293-5.293H24v-1z" data-name="Left"/></svg>
+        Return Home
+      </button>
+      <h1 class="align-top text-white font-bold lg:text-5xl text-2xl pt-10 pb-14 text-center">
+        All Projects
+      </h1>
+      <table class="table-fixed w-full h-60 text-center">
+        <thead class="align-top text-white font-bold border-b-2 border-gray-600 text-lg">
+          <tr>
+            <th>Year</th>
+            <th>Name</th>
+            <th>Made With</th>
+            <th>Link</th>
+          </tr>
+        </thead>
+        <tbody v-for="project in projectData" class="text-white border-b-2 border-gray-600">
+          <tr>
+            <td class="opacity-80">{{ project.yearMade }}</td>
+            <td class="font-semibold">{{ project.projectName }}</td>
+            <td>
+              <table class="table-fixed mx-auto">
+                <td v-for="lang in project.languagesUsed" class="pl-2 opacity-80">{{ lang }}</td>
+              </table>
+            </td>
+            <td class="text-gray-400 hover:text-white hover:opacity-80 hover:cursor-pointer">{{ project.projectLink }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <style scoped>
 </style>
+
+<!-- TODO
+- Do a mobile design, instead of making the one desgin attempt to look ok on mobile make 2 designs one for mobile one for pc
+- Add a resume download/view button
+-->
